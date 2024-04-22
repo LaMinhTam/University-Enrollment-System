2956199c3c681ac0bd4a7739ac03070b4d7649b3
laminhtam <laminhtam5523@gmail.com>
2024-04-22 19:33:16 +0700

    add docker-compose.yml

--- UniEnrollSystemBE/student-service/src/main/java/vn/edu/iuh/fit/studentservice/controllers/StudentController.java
+++ UniEnrollSystemBE/student-service/src/main/java/vn/edu/iuh/fit/studentservice/controllers/StudentController.java
@@ -3,14 +3,22 @@ package vn.edu.iuh.fit.studentservice.controllers;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import vn.edu.iuh.fit.studentservice.DTO.StudentDTO;
 import vn.edu.iuh.fit.studentservice.models.Student;
+import vn.edu.iuh.fit.studentservice.services.StudentService;
 
 @RestController
 @RequestMapping("/student")
 public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping("/hello")
     public ResponseEntity<?> hellowrod() {
         ResponseWrapper response = new ResponseWrapper(200, "Hello world", new ResponseStudent("Nguyen Van A", "123 Nguyen Hue", "0123456789"));
@@ -19,7 +27,7 @@ public class StudentController {
 
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody StudentDTO student) {
-
+        studentService.save(student);
         return ResponseEntity.ok(student);
     }
 }
