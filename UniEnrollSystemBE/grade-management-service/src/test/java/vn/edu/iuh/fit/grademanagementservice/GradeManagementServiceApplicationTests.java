92a59f28c46e5f903c2c1807241cdcbde47c4488
laminhtam <laminhtam5523@gmail.com>
2024-04-29 23:20:12 +0700

    initialize grade management service

---
+++ UniEnrollSystemBE/grade-management-service/src/test/java/vn/edu/iuh/fit/grademanagementservice/GradeManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.grademanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
